ed1dd1f98f67fab7665a20d1e6d5bdac98044f5f
shubhamghule <ghuleshubham245@gmail.com>
2026-08-19 23:02:31 +0530

    Add StringProgramm class with reverseString method
    Added LogicBuilding.Strings.StringProgramm class with reverseString method to reverse and print strings. Updated Program.cs to use this class, replacing the original Hello World output.

---
+++ LogicBuilding/LogicBuilding/Strings/StringProgramm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicBuilding.Strings
+{
+    public class StringProgramm
+    {
+        public void reverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input string is null or empty.");
+                return;
+            }
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            Console.WriteLine($"Original String: {input}");
+            Console.WriteLine($"Reversed String: {reversedString}");
+        }
+    }
+}
