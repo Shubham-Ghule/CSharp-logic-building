e7ada1c5e1f66ed73e35bc2a584a02bd927f9044
shubhamghule <ghuleshubham245@gmail.com>
2026-08-22 21:37:23 +0530

    Add FindLargestElement class and update Program.cs
    Added FindLargestElement class in LogicBuilding.Arrays to find and print the largest element in an integer array. Updated Program.cs to use this class after string reversal logic.

---
+++ LogicBuilding/LogicBuilding/Arrays/FindLargestElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicBuilding.Arrays
+{
+    public class FindLargestElement
+    {
+        int[] arr = { 10, 83, 45, 92, 34, 15 };
+        public void FindLargest()
+        {
+            int largest = arr[0];
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+            }
+
+            Console.WriteLine($"The largest element in the array is: {largest}");
+
+        }
+    }
+}
--- LogicBuilding/LogicBuilding/Program.cs
+++ LogicBuilding/LogicBuilding/Program.cs
@@ -1,5 +1,9 @@
-﻿using LogicBuilding.Strings;
+﻿using LogicBuilding.Arrays;
+using LogicBuilding.Strings;
 
 Console.WriteLine("Hello, World!");
 StringProgramm stringProgramm = new StringProgramm();
 stringProgramm.reverseString("Hello, World!");
+
+FindLargestElement findLargestElement = new FindLargestElement();
+findLargestElement.FindLargest();
